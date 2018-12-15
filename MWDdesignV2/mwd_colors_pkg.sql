3afed8a84f4e4cbf465ec5e5463cded499b37128
C. Boom <yiriandur@yahoo.com>
2018-12-15 02:28:07 +0100

    finalize colors/materials v1 packages.

--- MWDdesignV2/mwd_colors_pkg.sql
+++ MWDdesignV2/mwd_colors_pkg.sql
@@ -4,67 +4,37 @@
  * Dit creert een single point of reference.
  * Run deze code om de package te creëren. 
  * De aanroep op bijv. pagina 3010 van de APEX applicatie, onder de LOV definitie zou zijn:
- * => RETURN MWD_COLORS_PKG.get_domain_values_query(P_DOMAIN_NAME => :P_DOMAIN_NAME_LOV);
+ * => RETURN MWD_COLORS_PKG.GET_COLORS_QUERY;
  */
 
 create or replace package MWD_COLORS_PKG as
 
-    function get_domain_values_query(
-        p_domain_name in varchar2 default null
-    ) return varchar2;
-    
-    function get_boolean_labels_query
-      return varchar2;
+    function get_colors_query
+		return varchar2;
         
-end MWD_COLORS_PKG;​
-
+end MWD_COLORS_PKG;
 / 
 
 create or replace package body MWD_COLORS_PKG as
 
-    function get_domain_values_query(
-        p_domain_name in varchar2 default null
-    ) return varchar2
-    is
-      v_domain_name varchar2(100);
-            v_query varchar2(5000);
-    begin
-        if (p_domain_name is not null)
-            then
-                v_domain_name := p_domain_name;
-                v_query := 'select 
-                                d.domain_value as display,
-                                d.domain_value as return
-                                from COLORS d
-                                where d.domain_name = '''|| UPPER(v_domain_name) ||'''
-                                order by 1';
-        else
-                v_query := 'select 
-                                d.domain_value as display,
-                                d.domain_value as return
-                                from COLORS d
-                                order by 1';
-        end if;
-            return v_query;
-    end get_domain_values_query;
-	
-	function get_boolean_labels_query
+    function get_colors_query
 		return varchar2
     is
-            v_query varchar2(5000);
-    begin	
-        v_query := 'select 
-						d.domain_value as display,
-						CASE 
-							WHEN d.domain_value = ''TRUE''
-								THEN ''Y''
-							WHEN d.domain_value = ''FALSE''
-								THEN ''N''
-						END as return
-						from COLORS d
-						where d.domain_name = ''BOOLEAN_LABEL''
-						order by 1';
-        return v_query;
-    end get_boolean_labels_query;
+        v_query varchar2(5000);
+    begin
+			v_query := 'select 
+                            nvl(
+                            CASE WHEN (c.color_type IS NULL OR c.color_type = ''MATTE'') THEN '''' ELSE c.color_type || '' '' END || 
+                            CASE WHEN (c.tint IS NULL or c.tint = ''NORMAL'') THEN '''' ELSE c.tint || '' '' END ||
+                            CASE WHEN c.base_color IS NULL THEN '''' ELSE c.base_color || '' '' END ||
+                            CASE WHEN c.added_color IS NULL THEN '''' ELSE c.added_color || '' '' END,
+                            c.name) as display,            
+                            c.name as return
+                            from COLORS c
+                            order by 1';
+            return v_query;
+    end get_colors_query;
 
-end MWD_COLORS_PKG;​
+end MWD_COLORS_PKG;
+
+/
